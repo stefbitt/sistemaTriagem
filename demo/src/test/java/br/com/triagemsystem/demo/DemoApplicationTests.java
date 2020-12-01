9de8beb95ddcd12725504e540dae648143234e9c
Stefany Silva <stefanyrecode01@gmail.com>
2020-11-30 21:59:48 -0300

    Inicializando projeto

---
+++ demo/src/test/java/br/com/triagemsystem/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.triagemsystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
